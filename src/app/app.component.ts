648941cd32d26cf63da112c2742164e0beeeed49
Giovanna Menegoli <52014893+GiMenegoli@users.noreply.github.com>
2020-03-28 19:02:48 -0300

    Criando primeiro component e adicionado bootstrap

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,6 +10,5 @@ import {
 
 export class AppComponent {
   title = 'Gerenciador de fotos';
-  description = "Porco";
-  url = "https://3.bp.blogspot.com/-2KML1b7vcUQ/WIkTc6v2yaI/AAAAAAAAEwg/S57KnnZJt1QX8BfJymYCLqAgmC0HLKTYQCLcB/s320/8b6062f54b39290253eaef36f154727e.jpg"
+ 
 }
