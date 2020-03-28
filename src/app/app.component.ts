78622660319f775b086a944d6e8f37deb38618ec
Giovanna Menegoli <52014893+GiMenegoli@users.noreply.github.com>
2020-03-28 18:26:53 -0300

    Adicionando imagem para teste

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,15 @@
-import { Component } from '@angular/core';
+import {
+  Component
+} from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
-  title = 'app';
+  title = 'Gerenciador de fotos';
+  description = "Porco";
+  url = "https://3.bp.blogspot.com/-2KML1b7vcUQ/WIkTc6v2yaI/AAAAAAAAEwg/S57KnnZJt1QX8BfJymYCLqAgmC0HLKTYQCLcB/s320/8b6062f54b39290253eaef36f154727e.jpg"
 }
