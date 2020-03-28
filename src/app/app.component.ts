8344e3329ed6b5205c4bbf2eb18e2178736388fa
Giovanna Menegoli <52014893+GiMenegoli@users.noreply.github.com>
2020-03-28 19:27:17 -0300

    Adicionando diretiva

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,5 +10,16 @@ import {
 
 export class AppComponent {
   title = 'Gerenciador de fotos';
+
+  photos = [
+    {
+      url: 'https://3.bp.blogspot.com/-2KML1b7vcUQ/WIkTc6v2yaI/AAAAAAAAEwg/S57KnnZJt1QX8BfJymYCLqAgmC0HLKTYQCLcB/s320/8b6062f54b39290253eaef36f154727e.jpg',
+      description: 'Porco'
+    },
+    {
+      url: 'https://img1.topimagens.com/ti/animais-fofos/animais-fofos_003.jpg',
+      description: 'Porquinho'
+    }
+  ];
  
 }
