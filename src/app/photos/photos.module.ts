0e744e06f49aaef55ae6e42411d75c1bcfee468e
Giovanna Menegoli <52014893+GiMenegoli@users.noreply.github.com>
2020-03-30 17:27:04 -0300

    Acionando submódulos

--- src/app/photos/photos.module.ts
+++ src/app/photos/photos.module.ts
@@ -1,28 +1,20 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
 import { PhotoFormComponent } from './photo-form/photo-form.component';
 
-import {PhotoListComponent} from './photo-list/photo-list.component';
+import { PhotoListModule } from './photo-list/photo-list.module';
+import { PhotoModule } from './photo/photo.module';
+import { PhotoFormModule } from './photo-form/photo-form.module';
 import { HttpClientModule } from '@angular/common/http';
-import { PhotoComponent } from './photo/photo.component';
-import { PhotosComponent } from './photo-list/photos/photos.component';
-import { FilterByDescription } from './photo-list/filter-by-description.pipe';
-import { LoadButtonComponent } from './photo-list/load-button/load-button.component';
+import { CommonModule } from '@angular/common';
 
 
 
 @NgModule({
-  declarations: [
-    PhotoComponent,
-    PhotoListComponent,
-    PhotoFormComponent,
-    PhotosComponent,
-    FilterByDescription,
-    LoadButtonComponent
-  ],
+  declarations: [],
   imports: [
-    HttpClientModule,
-    CommonModule
+    PhotoModule,
+    PhotoFormModule,
+    PhotoListModule
 ]
 
 })
