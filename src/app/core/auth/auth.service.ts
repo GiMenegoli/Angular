7326af27e9e70c701877622bb3b7b130d96e468e
Giovanna Menegoli <52014893+GiMenegoli@users.noreply.github.com>
2020-03-31 17:39:01 -0300

    Acessando headers

--- src/app/core/auth/auth.service.ts
+++ src/app/core/auth/auth.service.ts
@@ -1,17 +1,36 @@
-import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import {
+  Injectable
+} from '@angular/core';
+import {
+  HttpClient
+} from '@angular/common/http';
+import {
+  tap
+} from 'rxjs/operators';
 
 const API_URL = 'http://localhost:3000';
 
 @Injectable({
-    providedIn: 'root'
+  providedIn: 'root'
 })
 export class AuthService {
 
-    constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient) {}
 
-    authenticate(userName: string, password: string) {
+  authenticate(userName: string, password: string) {
 
-        return this.http.post(API_URL + '/user/login', { userName, password } )
-    }
-}
+      return this.http
+        .post(
+          API_URL + '/user/login', {
+            userName,
+            password
+          }, {
+            observe: 'response'
+          }
+        )
+        .pipe(tap(res => {
+              const authToken = res.headers.get('x-access-token');
+              console.log(`User ${userName} authenticated with token ${authToken}`)
+            }));
+            }
+        }
