d396da809516f2b42f2e7b3e33f9dad29e8417b7
Giovanna Menegoli <52014893+GiMenegoli@users.noreply.github.com>
2020-03-31 16:46:34 -0300

    Implementação login com API

--- src/app/home/signin/signin.component.ts
+++ src/app/home/signin/signin.component.ts
@@ -1,12 +1,22 @@
 import {
   Component,
-  OnInit
+  OnInit,
+  Input,
+  ElementRef,
+  ViewChild
 } from '@angular/core';
 import {
   FormGroup,
   FormBuilder,
   Validators
 } from '@angular/forms';
+import {
+  AuthService
+} from 'src/app/core/auth/auth.service';
+import {
+  Router
+} from '@angular/router';
+import { PlatformDetectorService } from 'src/app/core/platform-detector/platform-detector.service';
 
 @Component({
   templateUrl: './signin.component.html'
@@ -15,9 +25,16 @@ import {
 
 export class SignInComponent implements OnInit {
 
+  
   loginForm: FormGroup;
+  @ViewChild('userNameInput')
+  userNameInput: ElementRef<HTMLInputElement>;
 
-  constructor(private formBuilder: FormBuilder) {
+  constructor(
+    private formBuilder: FormBuilder,
+    private authService: AuthService,
+    private router: Router,
+    private platformDetectodService: PlatformDetectorService) {
 
   }
 
@@ -27,5 +44,23 @@ export class SignInComponent implements OnInit {
       password: ['', Validators.required]
     });
 
+  }
+  login() {
+
+    const userName = this.loginForm.get('userName').value;
+    const password = this.loginForm.get('password').value;
+    
+    this.authService
+    .authenticate(userName, password)
+    .subscribe(
+        () => this.router.navigate(['user', userName]),
+        err => {
+            console.log(err);
+            this.loginForm.reset();
+            this.platformDetectodService.isPlatformBrowser() &&
+            this.userNameInput.nativeElement.focus();
+            alert('Invalid user name or password');
+        }
+    );
   }
 }
