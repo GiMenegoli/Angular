9255e82ed26a8be0541acab84792f93880f000a5
Giovanna Menegoli <52014893+GiMenegoli@users.noreply.github.com>
2020-03-31 18:03:56 -0300

    Autenticação e token

--- src/app/core/auth/auth.service.ts
+++ src/app/core/auth/auth.service.ts
@@ -7,6 +7,7 @@ import {
 import {
   tap
 } from 'rxjs/operators';
+import { TokenService } from '../token/token.service';
 
 const API_URL = 'http://localhost:3000';
 
@@ -15,7 +16,10 @@ const API_URL = 'http://localhost:3000';
 })
 export class AuthService {
 
-  constructor(private http: HttpClient) {}
+  constructor(
+      private http: HttpClient,
+      private tokenService: TokenService
+      ) {}
 
   authenticate(userName: string, password: string) {
 
@@ -30,6 +34,7 @@ export class AuthService {
         )
         .pipe(tap(res => {
               const authToken = res.headers.get('x-access-token');
+              this.tokenService.setToken(authToken);
               console.log(`User ${userName} authenticated with token ${authToken}`)
             }));
             }
