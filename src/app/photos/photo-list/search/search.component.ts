400ac1456c5949700ddd8630ca3c1b1744830c8f
Giovanna Menegoli <52014893+GiMenegoli@users.noreply.github.com>
2020-03-30 22:51:59 -0300

    Ajuste import

--- src/app/photos/photo-list/search/search.component.ts
+++ src/app/photos/photo-list/search/search.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, OnDestroy, Output, EventEmitter, EventEmitter, Input } from '@angular/core';
+import { Component, OnInit, OnDestroy, Output, EventEmitter, Input } from '@angular/core';
 import { Subject } from 'rxjs';
 import { debounceTime } from 'rxjs/operators';
 
